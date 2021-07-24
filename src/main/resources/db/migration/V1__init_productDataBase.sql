029e26756dcc117c57607dc3ecff3b51df41d3b6
Evengarr <Ravasect@gmail.ru>
2021-07-24 10:54:48 +0300

    домашнее задание к 11 уроку (Основы веб-разработки на Spring Framework)

--- src/main/resources/db/migration/V1__init_productDataBase.sql
+++ src/main/resources/db/migration/V1__init_productDataBase.sql
@@ -23,3 +23,44 @@ VALUES ('aaa', 1.1),
        ('nnn', 55.9),
        ('ooo', 94.31),
        ('ppp', 888.9);
+
+create table users
+(
+    id       bigserial,
+    username varchar(30) not null,
+    password varchar(80) not null,
+    email    varchar(50) unique,
+    score    bigserial,
+    primary key (id)
+);
+
+create table roles
+(
+    id   serial,
+    name varchar(50) not null,
+    primary key (id)
+);
+
+create table users_roles
+(
+    user_id bigint not null,
+    role_id int    not null,
+    primary key (user_id, role_id),
+    foreign key (user_id) references users (id),
+    foreign key (role_id) references roles (id)
+);
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+insert into users (username, password, email, score)
+values
+--        user user
+('user', '$2y$12$KrzoIrtbd5clEOW8YchAMuZmwUuYLkuVJDw1Urh0Bf8QQqWBscmMK', 'user@gmail.com', 300),
+--         admin    admin
+('admin', '$2y$12$TfQDy9wG3yxrpFgOGL1OGuh4spwczrgfVTG9cJtz6nNmedZIOt0rm', 'admin@gmail.com', 250);
+
+insert into users_roles (user_id, role_id)
+VALUES (1, 1),
+       (2, 2);
