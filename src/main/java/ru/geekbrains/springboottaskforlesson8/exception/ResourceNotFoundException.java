afa863d0fdd0ea85c7345606ae5aee190d9bca5d
Evengarr <Ravasect@gmail.ru>
2021-07-25 19:40:30 +0300

    1

--- src/main/java/ru/geekbrains/springboottaskforlesson8/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.springboottaskforlesson8.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/geekbrains/springboottaskforlesson8/repositorues/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.springboottaskforlesson8.repositorues;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.springboottaskforlesson8.models.entities.Role;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
